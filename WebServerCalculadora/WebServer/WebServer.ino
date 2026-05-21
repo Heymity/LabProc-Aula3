270fe39333571518f3c70219120998bb14352ca9
Gabriel Pasquale R Scavone <gabriel.prscavone@gmail.com>
2026-05-20 23:39:48 -0300

    Web server

---
+++ WebServerCalculadora/WebServer/WebServer.ino
@@ -0,0 +1,120 @@
+#include <Arduino.h>
+#include <WebServer.h>
+#include <uri/UriRegex.h>
+#include <WiFi.h>
+
+#include "secrets.h"  // add WLAN Credentials in here.
+
+#include "esp_partition.h"  // to check existing data partitions in Flash memory
+
+#include <FS.h>        // File System for Web Server Files
+#include <LittleFS.h>  // Use LittleFSThis file system is used.
+#include <FFat.h>      // or.. FAT
+
+// mark parameters not used in example
+#define UNUSED __attribute__((unused))
+
+// TRACE output simplified, can be deactivated here
+#define TRACE(...) Serial.printf(__VA_ARGS__)
+
+// name of the server. You reach it using http://webserver
+#define HOSTNAME "webserver"
+
+// local time zone definition (Berlin)
+#define TIMEZONE "CET-1CEST,M3.5.0,M10.5.0/3"
+
+// need a WebServer for http access on port 80.
+WebServer server(80);
+
+// The file system in use...
+fs::FS *fsys = nullptr;
+
+// The text of builtin files are in this header file
+#include "builtinfiles.h"
+
+
+// ===== Simple functions used to answer simple GET requests =====
+
+// This function is called when the sysInfo service was requested.
+void calculator() {
+  String op = server.pathArg(0);
+  String a = server.pathArg(1);
+  String b = server.pathArg(2); 
+
+  int a = std::stoi(a);
+  int b = std::stoi(b);
+
+  int res = 0;
+  if (op == "p") {
+    res = a + b;
+  } else {
+    res = a - b;
+  }
+
+  server.send(200, "application/json", "{res:" + res + "}");
+}  // handleSysInfo()
+
+
+// Setup everything to make the webserver work.
+void setup(void) {
+  delay(3000);  // wait for serial monitor to start completely.
+
+  // Use Serial port for some trace information from the example
+  Serial.begin(115200);
+  Serial.setDebugOutput(false);
+
+  TRACE("Starting WebServer example...\n");
+
+  // allow to address the device by the given name e.g. http://webserver
+  WiFi.setHostname(HOSTNAME);
+
+  // start WiFI
+  WiFi.mode(WIFI_AP);
+  if (strlen(ssid) == 0) {
+    WiFi.begin();
+  } else {
+    WiFi.begin(ssid, passPhrase);
+  }
+
+  TRACE("Connect to WiFi...\n");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    TRACE(".");
+  }
+  TRACE("connected.\n");
+
+  // Ask for the current time using NTP request builtin into ESP firmware.
+  TRACE("Setup ntp...\n");
+  configTzTime(TIMEZONE, "pool.ntp.org");
+
+  TRACE("Register redirect...\n");
+
+  TRACE("Register service handlers...\n");
+
+  // register some REST services
+  server.on(UriRegex("/(p|m)/(\d+)/(\d+)"), HTTP_GET, calculator);
+
+  TRACE("Register file system handlers...\n");
+
+  // enable CORS header in webserver results
+  server.enableCORS(true);
+
+  TRACE("Register default (not found) answer...\n");
+
+  // handle cases when file is not found
+  server.onNotFound([]() {
+    // standard not found in browser.
+    server.send(404, "text/html", FPSTR(notFoundContent));
+  });
+
+  server.begin();
+
+  TRACE("open <http://%s> or <http://%s>\n", WiFi.getHostname(), WiFi.localIP().toString().c_str());
+}  // setup
+
+// run the server...
+void loop(void) {
+  server.handleClient();
+}  // loop()
+
+// end.
